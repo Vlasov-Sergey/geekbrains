559817cf24cee014bd5e1a92b3dfef4a05311a3d
Sergey Vlasov <sv@cert.kz>
2019-11-04 02:17:18 +0600

    Lesson 1 (styles and default values)

--- Javascript/les_01/js/main.js
+++ Javascript/les_01/js/main.js
@@ -1,20 +1,20 @@
 const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
+  {id: 1, title: 'Notebook', price: 20000, img:'notebook_air.jpg'},
+  {id: 2, title: 'Mouse', price: 1500, img:'no_pic.png'},
+  {id: 3, title: 'Keyboard', price: 5000, img:'no_img.png'},
   {id: 4, title: 'Gamepad', price: 4500},
+  {id: 5, img:'notebook_air.jpg'},
 ];
 
-const renderProduct = (title, price) => {
-  return `<div class="product-item">
+const renderProduct = (title='Other', price='0', img='no_pic.png') => `<div class="product-item">
             <h3>${title}</h3>
             <p>${price}</p>
+			<img src="./img/${img}" height="100">
           </div>`;
-};
+;
 
 const renderProducts = goodsList => {
-  const productsList = goodsList.map(good => renderProduct(good.title, good.price));
-  document.querySelector('.products').innerHTML = productsList;
+  document.querySelector('.products').innerHTML = goodsList.map(good => renderProduct(good.title, good.price, good.img));
 };
 
 renderProducts(products);
